18e17311104a3de9f1875497ce3922501fdbe527
malekcoders <alomayri970@outlook.com>
2023-12-22 13:13:08 +0300

    init

---
+++ java-queue/src/QueueArray.java
@@ -0,0 +1,11 @@
+public class QueueArray {
+  /*add your code here*/
+  public static void main(String[] args) {
+        QueueArray queueArray = new QueueArray();
+
+        queueArray.enQueue(100);
+        queueArray.enQueue(101);
+        queueArray.enQueue(102);
+        queueArray.display();
+    }
+}
